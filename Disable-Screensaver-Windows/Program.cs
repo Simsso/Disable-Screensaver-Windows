a9d6b465e713c0ec18b5ea2496e5fcdd44cb43f9
Timo Denk <github@timodenk.com>
2017-03-08 17:28:35 +0100

    Add code and executable

--- Disable-Screensaver-Windows/Program.cs
+++ Disable-Screensaver-Windows/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace Disable_Screensaver_Windows
 {
@@ -10,6 +9,42 @@ namespace Disable_Screensaver_Windows
     {
         static void Main(string[] args)
         {
+            Timer timer = new Timer();
+            // timer.Interval = 4 minutes
+            timer.Interval = (int)(TimeSpan.TicksPerMinute * 4 / TimeSpan.TicksPerMillisecond);
+            timer.Tick += (sender, timerArgs) => { Cursor.Position = new Point(Cursor.Position.X + 1, Cursor.Position.Y + 1); PreventSleep(); };
+            timer.Start();
+            Application.Run();
+
+            PreventSleep();
+        }
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_SYSTEM_REQUIRED = 0x00000001,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT   = 0x00000004,
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+        }
+
+        public static class SleepUtil
+        {
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+        }
+
+        public static void PreventSleep()
+        {
+            if (SleepUtil.SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
+                | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                | EXECUTION_STATE.ES_SYSTEM_REQUIRED
+                | EXECUTION_STATE.ES_AWAYMODE_REQUIRED) == 0) //Away mode for Windows >= Vista
+                SleepUtil.SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
+                    | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                    | EXECUTION_STATE.ES_SYSTEM_REQUIRED); //Windows < Vista, forget away mode
         }
     }
 }
