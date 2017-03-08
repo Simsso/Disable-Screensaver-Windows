cc3728c4eab8f4211be7fa99dd615827f14151bd
Timo Denk <github@timodenk.com>
2017-03-08 17:24:01 +0100

    Add project files.

---
+++ Disable-Screensaver-Windows/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disable_Screensaver_Windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
